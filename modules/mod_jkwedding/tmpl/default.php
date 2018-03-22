7cb20b7f2eb800e9f5e0f60ddcb7a070e1a8a6fb
knigherrant <knigherrant@gmail.com>
2018-03-22 13:34:01 +0700

    update module

---
+++ modules/mod_jkwedding/tmpl/default.php
@@ -0,0 +1,16 @@
+<?php
+/*
+# mod_jvdemo - JK Wedding
+# @version		1.0.0
+# ------------------------------------------------------------------------
+# author    PHPKungfu Solutions Co
+# copyright Copyright (C) 2014 phpkungfu.club. All Rights Reserved.
+# @license - http://www.gnu.org/licenses/gpl-2.0.html GNU/GPL or later.
+# Websites: http://www.phpkungfu.club
+# Technical Support:  http://www.phpkungfu.club/my-tickets.html
+-------------------------------------------------------------------------*/
+
+defined('_JEXEC') or die('Restricted access');
+JHtml::_('formbehavior.chosen', '#demo-fonts select');
+$document   = JFactory::getDocument();
+?>
